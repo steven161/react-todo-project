3605bc9a21f3a8f8814e70b69f64ed477c5f27c7
Steven <steven.nguyen161@gmail.com>
2021-06-21 15:36:55 +0700

    change router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import TodoContainer from "./components/TodoContainer";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
